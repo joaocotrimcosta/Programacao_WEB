d8dbb733ff036caa81970699cc34bd279aeb79c0
joaocotrimcosta <115737003+joaocotrimcosta@users.noreply.github.com>
2023-11-01 12:07:33 +0000

    Post inicial

---
+++ _primeiro/index.js
@@ -0,0 +1,13 @@
+console.log ("ficheiro js carregado")
+
+spans = document.getElementsByTagName("span")
+
+console.log(spans)
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+
+    element.innerText = index 
+
+    
+}
